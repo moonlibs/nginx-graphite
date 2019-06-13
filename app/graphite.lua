18dee711a2dc7a23e1b3096e2de8c011cb94aa91
Victoria Suslova <suslova_vichka@mail.ru>
2019-06-13 10:53:07 +0300

    default functions for graphite metrics

--- app/graphite.lua
+++ app/graphite.lua
@@ -3,43 +3,63 @@ local ngx    = require 'ngx'
 
 local M = {}
 
-function M:transmit(metric_msg, response_time)
-	local msg  = metric_msg.." "..tostring(response_time).." "..tostring(ngx.time()).."\n"
+function M:transmit(metric_msg)
+	local msg  = string.format("%s %s %s\n", metric_msg, tostring(ngx.time()))
 	self.sock:send(msg)
 end
-
-local ok, metric = pcall(require, 'metric')
 
 function M:new(params)
 	assert(params and type(params) == 'table', "require params and params must be table")
 	assert(params.host, "require host")
 	assert(params.port and tonumber(params.port), "require port and port must be number")
-	
-	self.use_default_qualifier = params.use_default_qualifier
-	if not ok or not metric or not metric.qualifier then
-		if params.use_default_qualifier then
-			metric = {}
-			metric.qualifier = function()
-				return {
-					operation = ngx.var.request_uri:gsub('^/', ''):gsub('/', '-');
-					first_prefix  = M.prefix.first;
-					second_prefix = M.prefix.second;
-					tx = tonumber(ngx.var.body_bytes_sent) or 0;
-					rx = tonumber(ngx.var.content_length)  or 0;
-				}
-			end
-		else
-			error("Not defining qualifier method in metric module: "..tostring(metric))
-		end
-	end
-	
-	local success, r = pcall(function()
-		self.use_default_qualifier = params.use_default_qualifier
-		self.prefix = {
-			first  = params.prefix and params.prefix.first or '';
-			second = params.prefix and params.prefix.second or '';
-		}
+	assert(params.prefix and type(params.prefix) == 'function', "require prefix and prefix must be function")
 
+	local graph  = {
+		metric = {};
+		prefix = params.prefix;
+	}
+
+	if params.use_default_metrics then
+		graph.metric = {
+			operation_qualifier = function()
+				return ngx.var.request_uri:gsub('^/', ''):gsub('/', '-');
+			end;
+			form = {
+				hits = function(operation)
+					local response_time = ngx.now() - ngx.req.start_time()
+					return string.format('%s.hits.%s.%s %s', self.prefix(), operation, ngx.status, response_time)
+				end;
+				traffic_tx = function(operation)
+					local tx = tonumber(ngx.var.body_bytes_sent) or 0;
+					if tx ~= 0 then
+						return string.format('%s.traffic.%s.tx %s', self.prefix(), operation, tx)
+					end
+					return
+				end;
+				traffic_rx = function(operation)
+					local rx = tonumber(ngx.var.content_length) or 0;
+					if rx ~= 0 then
+						return string.format('%s.traffic.%s.rx %s', self.prefix(), operation, rx)
+					end
+					return
+				end;
+			};
+		}
+	else
+		local ok, res = pcall(require, 'metric')
+		if not ok or not res then
+			error(string.format("Impossible to load metric module. Res=%s", tostring(res)))
+		end
+		if not res.operation_qualifier then
+			error("No operation_qualifier function in metric module")
+		end
+		if not res.form then
+			error("No form function in metric module")
+		end
+		graph.metric = res
+	end
+
+	local success, result = pcall(function(params)
 		local sock = socket:new{
 			host = params.host;
 			port = tonumber(params.port);
@@ -47,15 +67,11 @@ function M:new(params)
 		sock:open()
 		return sock;
 	end, params)
-	
-	local graph = {}
-	
-	if not success then
-		error(r)
-	else
-		graph.sock = r
+	if not success or not result then
+		error(result)
 	end
-	
+
+	graph.sock = result
 	setmetatable(graph , self)
 	self.__index = self
 	
@@ -64,25 +80,12 @@ function M:new(params)
 end
 
 function M:send()
-	local response_time = ngx.now() - ngx.req.start_time()
-	local success, metrics = pcall(metric.qualifier)
-	if not success then return ngx.log(ngx.ERR, metrics) end
-	local metric_msg = string.format('%s.hits.%s.%s.%s',
-		metrics.first_prefix, metrics.second_prefix, metrics.operation, ngx.status
-	)
-	
-	self:transmit(metric_msg, response_time)
-	if metrics.rx ~= 0 then
-		local rx_msg = string.format('%s.traffic.%s.%s.rx',
-			metrics.first_prefix, metrics.second_prefix, metrics.operation
-		)
-		self:transmit(rx_msg, metrics.rx)
-	end
-	if metrics.tx ~= 0 then
-		local tx_msg = string.format('%s.traffic.%s.%s.tx',
-			metrics.first_prefix, metrics.second_prefix, metrics.operation
-		)
-		self:transmit(tx_msg, metrics.tx)
+	local success, operation = pcall(self.metric.operation_qualifier)
+	if not success then return ngx.log(ngx.ERR, operation) end
+	local host = string.gsub(ngx.var.host, "%.", "-")
+	for name, form_message in pairs(self.metric.form) do
+		local msg = form_message(operation, host)
+		if msg then self:transmit(msg) end
 	end
 end
 
